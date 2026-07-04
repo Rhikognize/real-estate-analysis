4642c2107955b03eb6b6def222207948804dea83
Smoshell <smoshell9@gmail.com>
2026-07-04 17:56:19 +0300

    Added Power BI dashboard. Sorted feature importance and added top_10 as a view for page3 in dashboard

--- scraper/load_to_sql.py
+++ scraper/load_to_sql.py
@@ -11,6 +11,9 @@ with open("sql/queries/01_create_view.sql") as f:
     query = f.read()
 
 conn.execute(query)
+with open("sql/queries/03_top10.sql", encoding="utf-8") as f:
+    query = f.read()
+conn.execute(f"CREATE VIEW IF NOT EXISTS top_10 AS  {query}")
 conn.commit()
 conn.close()
 
